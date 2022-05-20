91c09f7d60273774f6e32cd7e9fe9052280ebdd7
RaymundoH21 <71298292+RaymundoH21@users.noreply.github.com>
2022-05-19 18:33:26 -0700

    Update base proyecto

---
+++ EscuelaWeb/EscuelaWeb/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace EscuelaWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.admisionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarSemestreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tarifaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detallesEstudianteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buscarEstudianteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detalleIndividualToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(953, 433);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Escolar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(884, 376);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 57);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Manejo del";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(884, 488);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(422, 342);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(262, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 35);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(202, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 35);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(215, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(26, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(241, 57);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Contraseña";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(25, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 57);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Usuario";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::EscuelaWeb.Properties.Resources.close;
+            this.pictureBox1.Image = global::EscuelaWeb.Properties.Resources.close;
+            this.pictureBox1.Location = new System.Drawing.Point(1768, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.admisionToolStripMenuItem,
+            this.tarifaToolStripMenuItem,
+            this.detallesEstudianteToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1818, 37);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // admisionToolStripMenuItem
+            // 
+            this.admisionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaToolStripMenuItem,
+            this.cargarSemestreToolStripMenuItem});
+            this.admisionToolStripMenuItem.Name = "admisionToolStripMenuItem";
+            this.admisionToolStripMenuItem.Size = new System.Drawing.Size(114, 33);
+            this.admisionToolStripMenuItem.Text = "Admision";
+            // 
+            // nuevaToolStripMenuItem
+            // 
+            this.nuevaToolStripMenuItem.Name = "nuevaToolStripMenuItem";
+            this.nuevaToolStripMenuItem.Size = new System.Drawing.Size(245, 34);
+            this.nuevaToolStripMenuItem.Text = "Nueva Admision";
+            // 
+            // cargarSemestreToolStripMenuItem
+            // 
+            this.cargarSemestreToolStripMenuItem.Name = "cargarSemestreToolStripMenuItem";
+            this.cargarSemestreToolStripMenuItem.Size = new System.Drawing.Size(245, 34);
+            this.cargarSemestreToolStripMenuItem.Text = "Cargar Semestre";
+            // 
+            // tarifaToolStripMenuItem
+            // 
+            this.tarifaToolStripMenuItem.Name = "tarifaToolStripMenuItem";
+            this.tarifaToolStripMenuItem.Size = new System.Drawing.Size(79, 33);
+            this.tarifaToolStripMenuItem.Text = "Tarifa";
+            // 
+            // detallesEstudianteToolStripMenuItem
+            // 
+            this.detallesEstudianteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buscarEstudianteToolStripMenuItem,
+            this.detalleIndividualToolStripMenuItem});
+            this.detallesEstudianteToolStripMenuItem.Name = "detallesEstudianteToolStripMenuItem";
+            this.detallesEstudianteToolStripMenuItem.Size = new System.Drawing.Size(206, 33);
+            this.detallesEstudianteToolStripMenuItem.Text = "Detalles Estudiante";
+            // 
+            // buscarEstudianteToolStripMenuItem
+            // 
+            this.buscarEstudianteToolStripMenuItem.Name = "buscarEstudianteToolStripMenuItem";
+            this.buscarEstudianteToolStripMenuItem.Size = new System.Drawing.Size(259, 34);
+            this.buscarEstudianteToolStripMenuItem.Text = "Buscar Estudiante";
+            // 
+            // detalleIndividualToolStripMenuItem
+            // 
+            this.detalleIndividualToolStripMenuItem.Name = "detalleIndividualToolStripMenuItem";
+            this.detalleIndividualToolStripMenuItem.Size = new System.Drawing.Size(259, 34);
+            this.detalleIndividualToolStripMenuItem.Text = "Detalle Individual";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::EscuelaWeb.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(1818, 1021);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label4;
+        private Label label3;
+        private PictureBox pictureBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem admisionToolStripMenuItem;
+        private ToolStripMenuItem nuevaToolStripMenuItem;
+        private ToolStripMenuItem cargarSemestreToolStripMenuItem;
+        private ToolStripMenuItem tarifaToolStripMenuItem;
+        private ToolStripMenuItem detallesEstudianteToolStripMenuItem;
+        private ToolStripMenuItem buscarEstudianteToolStripMenuItem;
+        private ToolStripMenuItem detalleIndividualToolStripMenuItem;
+    }
+}
---
+++ EscuelaWeb/EscuelaWeb/Form1.cs
@@ -0,0 +1,25 @@
+namespace EscuelaWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
